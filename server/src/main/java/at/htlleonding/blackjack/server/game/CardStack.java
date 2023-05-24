b47316c4873d476a55a08e7b1b9dff7a1b6ea23d
wfee2000 <ilmingwinnie19@gmail.com>
2023-05-25 01:31:28 +0200

    feat: create cardStack

---
+++ server/src/main/java/at/htlleonding/blackjack/server/game/CardStack.java
@@ -0,0 +1,49 @@
+package at.htlleonding.blackjack.server.game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class CardStack {
+    private final Stack<Card> stack;
+
+    public CardStack(boolean isFull) {
+        stack = new Stack<>();
+        if (isFull) {
+            initStack();
+        }
+    }
+
+    private void initStack() {
+        List<Card> cards = new ArrayList<>();
+
+        Arrays.stream(Sign.values()).forEach(
+                sign -> Arrays.stream(Value.values()).forEach(value -> cards.add(new Card(sign, value))));
+
+        while (!cards.isEmpty()) {
+            stack.push(cards.remove((int)(Math.random() * cards.size())));
+        }
+    }
+
+    public Card takeCard() {
+        return stack.pop();
+    }
+
+    public List<Card> takeCards(int amount) {
+        return stack.stream().limit(amount).toList();
+    }
+
+    public void putCardsBack(List<Card> cards) {
+        stack.addAll(cards);
+        shuffle();
+    }
+
+    public void shuffle() {
+        List<Card> cards = new ArrayList<>(stack.stream().toList());
+
+        while (!cards.isEmpty()) {
+            stack.push(cards.remove((int)(Math.random() * cards.size())));
+        }
+    }
+}

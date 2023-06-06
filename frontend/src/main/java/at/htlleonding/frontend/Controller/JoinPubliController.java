ec9a813442fb1ef4d567d7582edad6ba759e0f1f
LukaCvijic <lukacvijic@outlook.com>
2023-06-06 11:17:14 +0200

    Added join fxml and updated other fxml

---
+++ frontend/src/main/java/at/htlleonding/frontend/Controller/JoinPubliController.java
@@ -0,0 +1,14 @@
+package at.htlleonding.frontend.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class JoinPubliController {
+    @FXML
+    public ListView lobbyListView;
+
+    public void initialize(){
+        // get all open Lobbies
+        //put in listView
+    }
+}

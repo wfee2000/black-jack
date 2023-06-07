c00d9dd1ccf2fe768bc1724415cde71d4fcd6383
wfee2000 <ilmingwinnie19@gmail.com>
2023-06-07 08:48:36 +0200

    feat: add new features - notify sockets on player (join|remove) - let player set bet

--- server/src/main/java/at/htlleonding/blackjack/server/game/Player.java
+++ server/src/main/java/at/htlleonding/blackjack/server/game/Player.java
@@ -1,28 +1,29 @@
 package at.htlleonding.blackjack.server.game;
 
 import at.htlleonding.blackjack.server.ClientThreadHandler;
+import at.htlleonding.blackjack.server.contents.CardContent;
+import at.htlleonding.blackjack.server.contents.MessageContent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private int coins;
-
     private int bet;
-
+    public int getBet() {
+        return bet;
+    }
     public boolean isOut() {
         return isOut;
     }
-
     public void isOut(boolean isOut) {
         this.isOut = isOut;
     }
     private boolean isOut;
-
     public boolean hasBlackJack() {
         return hasBlackJack;
     }
-
     public void hasBlackJack(boolean hasBlackJack) {
         this.hasBlackJack = hasBlackJack;
     }
@@ -44,6 +45,7 @@ public class Player {
             return false;
         }
 
+        hit(card);
         coins -= bet;
         bet *= 2;
         isOut = true;
@@ -52,10 +54,22 @@ public class Player {
 
     public void hit(Card card) {
         cards.add(card);
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("add",
+                    ClientThreadHandler.mapper.writeValueAsString(new CardContent(card)))));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean distribute(List<Card> cards) {
         this.cards.addAll(cards);
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("distributed",
+                    ClientThreadHandler.mapper.writeValueAsString(cards.stream().map(CardContent::new).toArray()))));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return Card.getSum(cards) == 21;
     }
 
@@ -64,20 +78,40 @@ public class Player {
     }
 
     public void triggerLoose() {
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("lost","")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         bet = 0;
     }
 
     public void triggerWin() {
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("lost","")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         coins += bet * 2;
         bet = 0;
     }
 
     public void triggerBlackjack() {
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("blackjack","")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         coins += bet * 3 / 2;
         bet = 0;
     }
 
     public void triggerDraw() {
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("draw","")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         coins += bet;
         bet = 0;
     }
@@ -95,6 +129,13 @@ public class Player {
     public List<Card> takeCardsAway() {
         List<Card> cards = this.cards;
         this.cards.clear();
+
+        try {
+            client.sendMessage(ClientThreadHandler.mapper.writeValueAsString(new MessageContent("take","")));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
         return cards;
     }
 

09c5a6fc32ca1e3e20df831439a12e42ee9c754b
wfee2000 <ilmingwinnie19@gmail.com>
2023-06-08 15:33:30 +0200

    chore: change rooms for frontend

--- server/src/main/java/at/htlleonding/blackjack/server/contents/CreateContent.java
+++ server/src/main/java/at/htlleonding/blackjack/server/contents/CreateContent.java
@@ -1,4 +1,4 @@
 package at.htlleonding.blackjack.server.contents;
 
-public record CreateContent(int rounds, int maxPlayers) {
+public record CreateContent(int rounds, int maxPlayers, String name, String password) {
 }

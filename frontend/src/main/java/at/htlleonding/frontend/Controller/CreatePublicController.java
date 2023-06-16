464344f3bda7dbe0ee9465cc2df56483d918d2b2
LukaCvijic <lukacvijic@outlook.com>
2023-06-16 23:03:11 +0200

    User can now create and join rooms

--- frontend/src/main/java/at/htlleonding/frontend/Controller/CreatePublicController.java
+++ frontend/src/main/java/at/htlleonding/frontend/Controller/CreatePublicController.java
@@ -1,9 +1,20 @@
 package at.htlleonding.frontend.Controller;
 
+import at.htlleonding.frontend.HelloApplication;
+import at.htlleonding.frontend.SessionHandler.SessionHandler;
+import at.htlleonding.frontend.model.CreateContent;
+import at.htlleonding.frontend.model.MessageContent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
 public class CreatePublicController {
     @FXML
@@ -18,6 +29,45 @@ public class CreatePublicController {
         int rounds = (int) roundSlider.getValue();
         int players = (int) playerSlider.getValue();
 
-        // enter room but in waiting mode
+        Socket socket = SessionHandler.getInstance().getSocket();
+
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            // Prepare json
+            String jsonString = mapper.writeValueAsString(
+                    new MessageContent("open", mapper.writeValueAsString(new CreateContent(
+                            String.valueOf(rounds),
+                            String.valueOf(players),
+                            SessionHandler.getInstance().getUserName(),
+                            "")))
+            );
+
+            // prepare printStream
+            PrintStream printStream = new PrintStream(socket.getOutputStream(), true);
+            // send json
+            printStream.println(jsonString);
+            // prepare reader
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // wait for reader
+            String output = clientReader.readLine(); // first time I get weird response
+
+            output = clientReader.readLine();
+
+            System.out.println(output);
+
+            if(output != null && output.equals("Success")){
+                // load into game
+                HelloApplication.setStageTo("game.fxml");
+            }
+            else{
+                Alert full = new Alert(Alert.AlertType.INFORMATION, "Sorry something went wrong");
+
+                full.showAndWait();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- frontend/src/main/java/at/htlleonding/frontend/Controller/JoinPrivateController.java
+++ frontend/src/main/java/at/htlleonding/frontend/Controller/JoinPrivateController.java
@@ -1,12 +1,22 @@
 package at.htlleonding.frontend.Controller;
 
+import at.htlleonding.frontend.HelloApplication;
 import at.htlleonding.frontend.SessionHandler.SessionHandler;
+import at.htlleonding.frontend.model.MessageContent;
+import at.htlleonding.frontend.model.RoomContent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JoinPrivateController {
 
@@ -22,6 +32,7 @@ public class JoinPrivateController {
 
         String name = nameField.getText();
         String password = pwdField.getText();
+
         Socket socket = SessionHandler.getInstance().getSocket();
 
         if(name.isEmpty() || password.isEmpty() ||
@@ -38,16 +49,41 @@ public class JoinPrivateController {
 
         try {
 
-            // check with database
+            RoomContent room = getRoom(socket, name, password);
 
-            var output = "";
-
-            if (output != null && output.equals("Connected")) {
-                //HelloApplication.setStageTo("home.fxml");
-            } else {
+            if (room == null){
                 Alert alert = new Alert(Alert.AlertType.WARNING);
 
                 alert.setContentText("Wrong lobbyname or password!");
+
+                alert.showAndWait();
+                return;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            // Prepare json
+            String jsonString = mapper.writeValueAsString(
+                    new MessageContent("join", room.id())
+            );
+
+            // prepare printStream
+            PrintStream printStream = new PrintStream(socket.getOutputStream(), true);
+            // send json
+            printStream.println(jsonString);
+            // prepare reader
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // wait for reader
+            String output = clientReader.readLine(); // first time I get weird response
+
+            output = clientReader.readLine();
+
+
+            if (output != null && output.equals("Success")) {
+                HelloApplication.setStageTo("game.fxml");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+
+                alert.setContentText("Something went wrong");
 
                 alert.showAndWait();
             }
@@ -55,5 +91,44 @@ public class JoinPrivateController {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public RoomContent getRoom(Socket socket, String name, String password){
+
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            // Prepare json
+            String jsonString = mapper.writeValueAsString(
+                    new MessageContent("rooms", mapper.writeValueAsString(""))
+            );
+
+            // prepare printStream
+            PrintStream printStream = new PrintStream(socket.getOutputStream(), true);
+            // send json
+            printStream.println(jsonString);
+            // prepare reader
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // wait for reader
+            String output = clientReader.readLine();
+
+            MessageContent messageContent = mapper.readValue(output, MessageContent.class);
+
+            String content = messageContent.content();
+
+            RoomContent[] allRooms = mapper.readValue(content, RoomContent[].class);
+
+            for (RoomContent room : allRooms) {
+                if(room.password().equals(password) && room.name().equals(name))
+                {
+                    return room;
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
     }
 }

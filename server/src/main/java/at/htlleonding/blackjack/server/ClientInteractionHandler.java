b9e35455208d22dad7ed2afd4f67783b33c87a6e
wfee2000 <ilmingwinnie19@gmail.com>
2023-06-17 13:37:48 +0200

    fix: add proper threading for client interactions

---
+++ server/src/main/java/at/htlleonding/blackjack/server/ClientInteractionHandler.java
@@ -0,0 +1,29 @@
+package at.htlleonding.blackjack.server;
+
+import at.htlleonding.blackjack.server.contents.MessageContent;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ClientInteractionHandler extends Thread {
+    private final ClientThreadHandler threadHandler;
+    private MessageContent clientMessage;
+    private PrintWriter clientMessagesOut;
+
+    public ClientInteractionHandler(ClientThreadHandler clientThreadHandler, MessageContent clientMessage,
+                                    PrintWriter clientMessagesOut) {
+        this.threadHandler = clientThreadHandler;
+        this.clientMessage = clientMessage;
+        this.clientMessagesOut = clientMessagesOut;
+    }
+    @Override
+    public void run() {
+        try {
+            value = threadHandler.processClientInteraction(clientMessage, clientMessagesOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean value;
+}
